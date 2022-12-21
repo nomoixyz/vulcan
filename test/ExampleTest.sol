edcbcedc24e1d59e575d9b119bff939e82c4980a
Vicente Dragicevic <vdrg@protonmail.ch>
2022-12-21 15:30:55 -0300

    Fix extended test

--- test/ExampleTest.sol
+++ test/ExampleTest.sol
@@ -3,13 +3,23 @@ pragma solidity ^0.8.13;
 import { Test, expect, _T } from  "../src/Sest.sol";
 
 library TestExtension {
-    function increaseTime(_T memory self, uint256 increase) internal returns(_T memory) {
+    function increaseBlockTimestamp(_T storage self, uint256 increase) internal returns(_T memory) {
         self.setBlockTimestamp(block.timestamp + increase);
         return self;
     }
 }
 
+using TestExtension for _T;
+
 contract ExampleTest is Test {
+    function testIncreaseTime() external {
+        uint256 increase = 1000;
+        uint256 current = block.timestamp;
+        test.increaseBlockTimestamp(increase);
+
+        expect(block.timestamp).toEqual(current + increase);
+    }
+
     function testSetBlockNumber() external {
         uint256 blockNumber = block.number + 1000;
         test.setBlockNumber(blockNumber);
