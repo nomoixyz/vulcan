2889309765afff9354e7df7738006709cf8bda73
gnkz <gnkz.dev@gmail.com>
2023-02-23 10:21:52 -0300

    fix: fix import

--- test/Context.t.sol
+++ test/Context.t.sol
@@ -1,6 +1,6 @@
 pragma solidity >=0.8.13 <0.9.0;
 
-import {Test, expect, VulcanVm, ctx, vulcan} from "../src/lib.sol";
+import {Test, expect, commands, ctx} from "../src/test.sol";
 
 contract ContextTest is Test {
     function testItCanSetTheBlockTimestamp(uint256 blockTimestamp) external {
