1ed8f0728b97e07a3194b60e024559e54224ee3f
Joshua Trujillo <Jtriley15@gmail.com>
2023-04-04 13:09:24 -0700

    add tests

--- test/Huff.t.sol
+++ test/Huff.t.sol
@@ -4,23 +4,24 @@ import {Test, expect, Command, console, huff, Huffc} from "../src/test.sol";
 
 bytes32 constant SLOT = 0x0000000000000000000000000000000000000000000000000000000000000001;
 bytes32 constant OTHER_SLOT = 0x0000000000000000000000000000000000000000000000000000000000000002;
+string constant SLOT_AS_STRING = "SLOT=0x0000000000000000000000000000000000000000000000000000000000000001";
 string constant SLOTS_AS_STRING =
     "SLOT=0x0000000000000000000000000000000000000000000000000000000000000001 OTHER_SLOT=0x0000000000000000000000000000000000000000000000000000000000000002";
 
 contract HuffTest is Test {
     using huff for *;
 
-    function testToCommand() external {
-    Command memory command = huff.create()
-        .setCompilerPath("diffhuff")
-        .setFilePath("./filePath.huff")
-        .setOutputPath("./outputPath.json")
-        .setMainName("ALT_MAIN")
-        .setConstructorName("ALT_CONSTRUCTOR")
-        .setOnlyRuntime(true)
-        .addConstantOverride("SLOT", SLOT)
-        .addConstantOverride("OTHER_SLOT", OTHER_SLOT)
-        .toCommand();
+    function testToCommandAllSet() external {
+        Command memory command = huff.create()
+            .setCompilerPath("diffhuff")
+            .setFilePath("./filePath.huff")
+            .setOutputPath("./outputPath.json")
+            .setMainName("ALT_MAIN")
+            .setConstructorName("ALT_CONSTRUCTOR")
+            .setOnlyRuntime(true)
+            .addConstantOverride("SLOT", SLOT)
+            .addConstantOverride("OTHER_SLOT", OTHER_SLOT)
+            .toCommand();
 
         expect(command.inputs.length).toEqual(12);
         expect(command.inputs[0]).toEqual("diffhuff");
@@ -37,8 +38,32 @@ contract HuffTest is Test {
         expect(command.inputs[11]).toEqual("./filePath.huff");
     }
 
+    function testToCommandConstantOverrideSingle() external {
+        Command memory command = huff.create()
+            .setFilePath("./filePath.huff")
+            .addConstantOverride("SLOT", SLOT)
+            .toCommand();
+
+        expect(command.inputs.length).toEqual(5);
+        expect(command.inputs[0]).toEqual("huffc");
+        expect(command.inputs[1]).toEqual("-c");
+        expect(command.inputs[2]).toEqual(SLOT_AS_STRING);
+        expect(command.inputs[3]).toEqual("-b");
+        expect(command.inputs[4]).toEqual("./filePath.huff");
+    }
+
+    function testToCommandMinimumSet() external {
+        Command memory command = huff.create().setFilePath("./filePath.huff").toCommand();
+
+        expect(command.inputs.length).toEqual(3);
+        expect(command.inputs[0]).toEqual("huffc");
+        expect(command.inputs[1]).toEqual("-b");
+        expect(command.inputs[2]).toEqual("./filePath.huff");
+    }
+
     function testCompile() external {
-        expect(huff.create().setFilePath("./test/mocks/Getter.huff").compile()).toEqual(
+        bytes memory initcode = huff.create().setFilePath("./test/mocks/Getter.huff").compile();
+        expect(initcode).toEqual(
             hex"602e8060093d393df360003560e01c806360fe47b11461001b57636d4ce63c14610022575b6004356000555b60005460005160206000f3"
         );
     }
