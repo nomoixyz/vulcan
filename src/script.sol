07b2f1d3c45bbaf1f89b6b5d56e8e8a0c886e6ec
gnkz <gnkz.dev@gmail.com>
2023-08-22 15:45:23 -0400

    chore: export huff from script.sol

--- src/script.sol
+++ src/script.sol
@@ -17,6 +17,7 @@ import {config, Rpc} from "./_modules/Config.sol";
 import {fmt} from "./_modules/Fmt.sol";
 import {format} from "./_utils/format.sol";
 import {println} from "./_utils/println.sol";
+import {huff, Huffc} from "./_modules/Huff.sol";
 
 contract Script {
     bool public IS_SCRIPT = true;
