7babc2e774d71db25fece34d2c1ee760f0d55e2b
gnkz <gnkz.dev@gmail.com>
2023-08-22 14:42:23 -0400

    docs: add docs for the `Fe` module.

---
+++ docs/src/reference/fe.md
@@ -0,0 +1,47 @@
+# Fe
+
+#### **`create() → (Fe)`**
+
+Creates a new `Fe` struct with the following defaults.
+
+```solidity
+Fe({
+    compilerPath: "fe",
+    filePath: "",
+    emitOptions: "",
+    outputDir: "",
+    overwrite: false
+});
+```
+
+> Notice: The `filePath` is the only required field, if it is empty, `.build` and `.toCommand`
+> will revert.
+
+#### **`build(Fe self) → (bytes)`**
+
+Builds a binary file from a `.fe` file.
+
+#### **`toCommand(Fe self) → (Command)`**
+
+Converts the `Fe` struct into a `Command` struct.
+
+#### **`setCompilerPath(Fe self, string compilerPath) → (Fe)`**
+
+Overwrites the compiler path.
+
+#### **`setFilePath(Fe self, string filePath) → (Fe)`**
+
+Overwrites the file path.
+
+#### **`setOutputDir(Fe self, string outputDir) → (Fe)`**
+
+Overwrites the default artifacts directory.
+
+#### **`setEmitOptions(Fe memory self, string memory emitOptions) → (Fe)`**
+
+Overwrites the default emit options. `abi,bytecode` is the default value. Use `fe build --help` to
+get all the other possible values.
+
+#### **`setOverwrite(Fe memory self, bool overwrite) → (Fe)`**
+
+Sets the build command overwrite flag. If `true` the contents of `outputDir` will be overwritten.
--- src/_modules/Fe.sol
+++ src/_modules/Fe.sol
@@ -15,16 +15,21 @@ struct Fe {
 library fe {
     using strings for bytes32;
 
+    /// @dev Creates a new `Fe` struct with default values.
     function create() internal pure returns (Fe memory) {
         return Fe({compilerPath: "fe", filePath: "", emitOptions: "", outputDir: "", overwrite: false});
     }
 
+    /// @dev Builds a binary file from a `.fe` file.
+    /// @param self The `Fe` struct to build.
     function build(Fe memory self) internal returns (bytes memory) {
         bytes memory output = self.toCommand().run();
         // TODO: add error handling
         return output;
     }
 
+    /// @dev Transforms a `Fe` struct to a `Command` struct.
+    /// @param self The `Fe` struct to transform.
     function toCommand(Fe memory self) internal pure returns (Command memory) {
         Command memory command = commands.create(self.compilerPath);
         require(bytes(self.filePath).length > 0, "fe.toCommand: self.filePath not set");
@@ -41,26 +46,41 @@ library fe {
         return command;
     }
 
+    /// @dev Sets the `fe` compiler path.
+    /// @param self The `Fe` struct to modify.
+    /// @param compilerPath The new compiler path.
     function setCompilerPath(Fe memory self, string memory compilerPath) internal pure returns (Fe memory) {
         self.compilerPath = compilerPath;
         return self;
     }
 
+    /// @dev Sets the `fe` file path to build.
+    /// @param self The `Fe` struct to modify.
+    /// @param filePath The path to the `.fe` file to build.
     function setFilePath(Fe memory self, string memory filePath) internal pure returns (Fe memory) {
         self.filePath = filePath;
         return self;
     }
 
+    /// @dev Sets the `fe` build command emit options.
+    /// @param self The `Fe` struct to modify.
+    /// @param emitOptions The build command emit options.
     function setEmitOptions(Fe memory self, string memory emitOptions) internal pure returns (Fe memory) {
         self.emitOptions = emitOptions;
         return self;
     }
 
+    /// @dev Sets the `fe` build command output directory.
+    /// @param self The `Fe` struct to modify.
+    /// @param outputDir The directory where the binary file will be saved.
     function setOutputDir(Fe memory self, string memory outputDir) internal pure returns (Fe memory) {
         self.outputDir = outputDir;
         return self;
     }
 
+    /// @dev Sets the `fe` build command overwrite flag.
+    /// @param self The `Fe` struct to modify.
+    /// @param overwrite If true it will overwrite the `outputDir with the new build binaries.
     function setOverwrite(Fe memory self, bool overwrite) internal pure returns (Fe memory) {
         self.overwrite = overwrite;
         return self;
