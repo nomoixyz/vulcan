d89b990b648fbf14c91fc5c31b9c5f0bfe9de770
Vicente Dragicevic <vdrg@protonmail.ch>
2023-08-28 11:37:21 -0400

    Export from test and script

--- src/_modules/Request.sol
+++ src/_modules/Request.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import {Command, commands} from "./Commands.sol";
 import {JsonObject, json as jsonModule} from "./Json.sol";
 
-struct Client {
+struct RequestClient {
     Command _cmd;
 }
 
@@ -17,54 +17,54 @@ struct RequestResult {
 library request {
     using request for *;
 
-    function create() internal pure returns (Client memory) {
+    function create() internal pure returns (RequestClient memory) {
         // Hide progress but return result or error
-        return Client({_cmd: commands.create("curl").args(["-S", "-s", "--fail-with-body"])});
+        return RequestClient({_cmd: commands.create("curl").args(["-S", "-s", "--fail-with-body"])});
     }
 
-    function send(Client memory self) internal returns (bytes memory) {
+    function send(RequestClient memory self) internal returns (bytes memory) {
         return self._cmd.run();
     }
 
-    function get(Client memory self, string memory url) internal pure returns (Client memory) {
-        return Client(self._cmd.arg(url));
+    function get(RequestClient memory self, string memory url) internal pure returns (RequestClient memory) {
+        return RequestClient(self._cmd.arg(url));
     }
 
     function get(string memory url) internal returns (bytes memory) {
         return create().get(url).send();
     }
 
-    function del(Client memory self, string memory url) internal pure returns (Client memory) {
-        return Client(self._cmd.args(["-X", "DELETE", url]));
+    function del(RequestClient memory self, string memory url) internal pure returns (RequestClient memory) {
+        return RequestClient(self._cmd.args(["-X", "DELETE", url]));
     }
 
-    function patch(Client memory self, string memory url) internal pure returns (Client memory) {
-        return Client(self._cmd.args(["-X", "PATCH", url]));
+    function patch(RequestClient memory self, string memory url) internal pure returns (RequestClient memory) {
+        return RequestClient(self._cmd.args(["-X", "PATCH", url]));
     }
 
-    function post(Client memory self, string memory url) internal pure returns (Client memory) {
-        return Client(self._cmd.args(["-X", "POST", url]));
+    function post(RequestClient memory self, string memory url) internal pure returns (RequestClient memory) {
+        return RequestClient(self._cmd.args(["-X", "POST", url]));
     }
 
-    function put(Client memory self, string memory url) internal pure returns (Client memory) {
-        return Client(self._cmd.args(["-X", "PUT", url]));
+    function put(RequestClient memory self, string memory url) internal pure returns (RequestClient memory) {
+        return RequestClient(self._cmd.args(["-X", "PUT", url]));
     }
 
-    function data(Client memory self, string memory _data) internal pure returns (Client memory) {
-        return Client(self._cmd.args(["-d", _data]));
+    function data(RequestClient memory self, string memory _data) internal pure returns (RequestClient memory) {
+        return RequestClient(self._cmd.args(["-d", _data]));
     }
 
-    function header(Client memory self, string memory _header) internal pure returns (Client memory) {
-        return Client(self._cmd.args(["-H", _header]));
+    function header(RequestClient memory self, string memory _header) internal pure returns (RequestClient memory) {
+        return RequestClient(self._cmd.args(["-H", _header]));
     }
 
-    function json(Client memory self, JsonObject memory obj) internal pure returns (Client memory) {
+    function json(RequestClient memory self, JsonObject memory obj) internal pure returns (RequestClient memory) {
         return self.json(obj.serialized);
     }
 
-    function json(Client memory self, string memory serialized) internal pure returns (Client memory) {
+    function json(RequestClient memory self, string memory serialized) internal pure returns (RequestClient memory) {
         return self.header("Content-Type: application/json").data(serialized);
     }
 }
 
-using request for Client global;
+using request for RequestClient global;
