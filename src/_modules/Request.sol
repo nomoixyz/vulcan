7935ea2272592bae19feefbbf0a7816e24f6e466
Vicente Dragicevic <vdrg@protonmail.ch>
2023-08-28 15:37:36 -0400

    Working request module

--- src/_modules/Request.sol
+++ src/_modules/Request.sol
@@ -1,6 +1,7 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import {println} from "../_utils/println.sol";
 import {Command, CommandResult, commands} from "./Commands.sol";
 import {JsonObject, json as jsonModule} from "./Json.sol";
 
@@ -10,29 +11,36 @@ struct RequestClient {
 
 struct RequestResult {
     uint256 statusCode;
-    bytes result;
-    string error;
+    bytes body;
+    RequestClient _client;
 }
+// bytes error;
 
 library request {
     using request for *;
 
     function create() internal pure returns (RequestClient memory) {
-        // Hide progress but return result or error
         return RequestClient({
+            /*
+             * Adapted from https://github.com/memester-xyz/surl/blob/034c912ae9b5e707a5afd21f145b452ad8e800df/src/Surl.sol#L90
+             *
+             * response=$(curl -s -w "\\n%{http_code}" "$@");
+             * body=$(sed "$ d" <<< "$response"  | tr -d "\\n");
+             * code=$(tail -n 1 <<< "$response");
+             * cast abi-encode "response(uint256,string)" "$code" "$body";
+             */
             _cmd: commands.create("bash").arg("-c").arg(
-                string.concat(
-                    'response=$(curl -s -w "\\n%{http_code}" "$@");',
-                    'body=$(echo "$response" | head -n -1);',
-                    'code=$(echo "$response" | tail -n 1);',
-                    'cast abi-encode "response(uint256,string)" "$code" "$body";'
-                )
-                )
+                'response=$(curl -s -w "\\n%{http_code}" "$@");body=$(sed "$ d" <<< "$response"  | tr -d "\\n");code=$(tail -n 1 <<< "$response");cast abi-encode "response(uint256,string)" "$code" "$body";'
+                ).arg("vulcan") // Add vulcan as parameter $0, so errors are reported as coming from vulcan
         });
     }
 
     function send(RequestClient memory self) internal returns (RequestResult memory) {
-        CommandResult 
+        CommandResult memory result = self._cmd.run();
+
+        (uint256 statusCode, bytes memory body) = abi.decode(result.stdout, (uint256, bytes));
+
+        return RequestResult({statusCode: statusCode, body: body, _client: self});
     }
 
     function get(string memory url) internal returns (RequestResult memory) {
@@ -74,6 +82,43 @@ library request {
     function json(RequestClient memory self, string memory serialized) internal pure returns (RequestClient memory) {
         return self.header("Content-Type: application/json").data(serialized);
     }
+
+    /// @dev Checks if a `RequestResult` returned an `ok` status code.
+    function isOk(RequestResult memory self) internal pure returns (bool) {
+        // TODO: what about 3xx ?
+        return self.statusCode >= 200 && self.statusCode < 300;
+    }
+
+    /// @dev Checks if a `CommandResult` struct is an error.
+    function isError(RequestResult memory self) internal pure returns (bool) {
+        return !self.isOk();
+    }
+
+    /// @dev Returns the output of a `CommandResult` or reverts if the result was an error.
+    function unwrap(RequestResult memory self) internal pure returns (bytes memory) {
+        string memory error;
+
+        if (self.isError()) {
+            // error = string.concat("Request failed ", self._client.toString());
+
+            if (self.body.length > 0) {
+                error = string.concat(error, ": ", string(self.body));
+            }
+        }
+
+        return expect(self, error);
+    }
+
+    /// @dev Returns the output of a `CommandResult` or reverts if the result was an error.
+    /// @param customError The error message that will be used when reverting.
+    function expect(RequestResult memory self, string memory customError) internal pure returns (bytes memory) {
+        if (self.isError()) {
+            revert(customError);
+        }
+
+        return self.body;
+    }
 }
 
 using request for RequestClient global;
+using request for RequestResult global;
--- test/_modules/Request.t.sol
+++ test/_modules/Request.t.sol
@@ -1,16 +1,27 @@
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.8.13 <0.9.0;
 
 import {
-    Test, expect, println, json, JsonObject, vulcan, request, RequestClient, RequestResult
+    Test,
+    expect,
+    println,
+    json,
+    JsonObject,
+    vulcan,
+    request,
+    RequestClient,
+    RequestResult,
+    commands
 } from "../../src/test.sol";
 
 contract RequestTest is Test {
     using vulcan for *;
 
     function testRequestGet() external {
-        RequestResult memory result = request.get("https://httpbin.org/get");
-        println(string(result.result));
-        expect(result.error).toEqual("");
+        RequestClient memory client = request.create().get("https://httpbin.org/get");
+
+        RequestResult memory result = client.send();
+        println(string(result.body));
         expect(result.statusCode).toEqual(200);
     }
 }
