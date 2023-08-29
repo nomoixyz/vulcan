dbe5ac1cf4155c7d10035b0e458c4dae09464dc1
Vicente Dragicevic <vdrg@protonmail.ch>
2023-08-29 16:34:39 -0400

    Remove immutable json

--- src/_modules/Json.sol
+++ src/_modules/Json.sol
@@ -9,10 +9,6 @@ struct JsonObject {
     string serialized;
 }
 
-struct ImmutableJson {
-    string serialized;
-}
-
 library json {
     /// @dev Parses a json object string by key and returns an ABI encoded value.
     /// @param jsonStr The json string.
