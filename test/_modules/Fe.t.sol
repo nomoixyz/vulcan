d00cd3f4dcc5025fc12521115e1774554674d4cd
Gonzalo <gnkz@users.noreply.github.com>
2023-08-30 18:50:29 -0400

    chore: make output dir optional when building Fe code (#149)

--- test/_modules/Fe.t.sol
+++ test/_modules/Fe.t.sol
@@ -1,30 +1,36 @@
 pragma solidity >=0.8.13 <0.9.0;
 
-import {Test, expect, commands, Command, fe, Fe, fs} from "../../src/test.sol";
+import {Test, expect, commands, Command, fe, Fe, fs, println, strings} from "../../src/test.sol";
 
 contract FeTest is Test {
     function testToCommandAllSet() external {
-        Command memory command = fe.create().setCompilerPath("difffe").setFilePath("./filePath.fe").setEmitOptions(
-            "abi"
-        ).setOutputDir("./feoutput").toCommand();
+        string memory filePath = "./filePath.fe";
+        Command memory command =
+            fe.create().setFilePath(filePath).setCompilerPath("difffe").setOutputDir("./feoutput").toCommand();
 
         expect(command.inputs.length).toEqual(7);
         expect(command.inputs[0]).toEqual("difffe");
         expect(command.inputs[1]).toEqual("build");
-        expect(command.inputs[2]).toEqual("-o");
-        expect(command.inputs[3]).toEqual("./feoutput");
-        expect(command.inputs[4]).toEqual("-e");
-        expect(command.inputs[5]).toEqual("abi");
-        expect(command.inputs[6]).toEqual("./filePath.fe");
+        expect(command.inputs[2]).toEqual("-e");
+        expect(command.inputs[3]).toEqual("bytecode");
+        expect(command.inputs[4]).toEqual("-o");
+        expect(command.inputs[5]).toEqual("./feoutput");
+        expect(command.inputs[6]).toEqual(filePath);
     }
 
     function testToCommandMinimumSet() external {
-        Command memory command = fe.create().setFilePath("./filePath.fe").toCommand();
+        string memory filePath = "./filePath.fe";
 
-        expect(command.inputs.length).toEqual(3);
+        Command memory command = fe.create().setFilePath(filePath).toCommand();
+
+        expect(command.inputs.length).toEqual(7);
         expect(command.inputs[0]).toEqual("fe");
         expect(command.inputs[1]).toEqual("build");
-        expect(command.inputs[2]).toEqual("./filePath.fe");
+        expect(command.inputs[2]).toEqual("-e");
+        expect(command.inputs[3]).toEqual("bytecode");
+        expect(command.inputs[4]).toEqual("-o");
+        expect(command.inputs[5]).toEqual("./out/__TMP/fe_builds");
+        expect(command.inputs[6]).toEqual("./filePath.fe");
     }
 
     function testCompile() external {
@@ -32,8 +38,18 @@ contract FeTest is Test {
             true
         ).build();
 
-        string memory result = fs.readFile("./test/fixtures/fe/output/GuestBook/GuestBook.bin");
+        string memory result = fs.readFile("./test/fixtures/fe/output/A/A.bin");
 
         expect(bytes(result).length).toBeGreaterThan(0);
+    }
+
+    function testCompileAndRead() external {
+        Fe memory feCmd = fe.create().setFilePath("./test/mocks/guest_book.fe").setOverwrite(true);
+
+        feCmd.build().unwrap();
+
+        string memory expectedBytecode = "600180600c6000396000f3fe00";
+
+        expect(string(feCmd.getBytecode("A"))).toEqual(expectedBytecode);
     }
 }
