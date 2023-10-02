806124adc8b979c89a17aad231d19a9ae8857852
Gonzalo <gnkz@users.noreply.github.com>
2023-10-02 13:12:24 -0300

    chore: move requests module out of experimental (#209)
    * chore: move requests module out of experimental
    
    * chore: add requests to script.sol

--- src/test.sol
+++ src/test.sol
@@ -29,6 +29,16 @@ import {removeSelector} from "./_utils/removeSelector.sol";
 import {Ok, StringResult, BoolResult, BytesResult, EmptyResult} from "./_modules/Result.sol";
 import {Error} from "./_modules/Error.sol";
 import {semver, Semver} from "./_modules/Semver.sol";
+import {
+    request,
+    Headers,
+    ResponseResult,
+    RequestResult,
+    Response,
+    Request,
+    RequestClient,
+    RequestBuilder
+} from "./_modules/Request.sol";
 
 // @dev Main entry point to Vulcan tests
 contract Test is InvariantsBase {
