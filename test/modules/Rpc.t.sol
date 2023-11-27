9c7d0dc65c1182efd2b73813edba30d49c25339c
gnkz <gnkz.dev@gmail.com>
2023-11-27 14:39:14 +0100

    test: add test for the `rpc` module

--- test/modules/Rpc.t.sol
+++ test/modules/Rpc.t.sol
@@ -1,20 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.13 <0.9.0;
 
-import {Test} from "vulcan/test.sol";
+import {Test, expect} from "vulcan/test.sol";
 import {rpc} from "vulcan/test/Rpc.sol";
-import {println} from "vulcan/utils.sol";
 import {Fork, forks} from "vulcan/test/Forks.sol";
 
 contract RpcTest is Test {
-    function testRpcCall() external {
-        forks.create("http://127.0.0.1:8545").select();
+    function testNetVersion() external {
+        forks.create("https://rpc.mevblocker.io/fast").select();
 
         string memory method = "eth_chainId";
         string memory params = "[]";
 
         bytes memory data = rpc.call(method, params);
 
-        println("Result {s}", abi.encode(data));
+        uint8 chainId;
+
+        assembly {
+            chainId := mload(add(data, 0x01))
+        }
+
+        expect(chainId).toEqual(1);
     }
 }
